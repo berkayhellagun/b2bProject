1023ce231b70f68c16f00c7dfeb8690546317436
berkayhellagun <berkayhellagun@hotmail.com>
2022-07-07 16:47:40 +0300

    auth controller create

--- WebAPI/Controllers/Concrete/AuthController.cs
+++ WebAPI/Controllers/Concrete/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers.Concrete
@@ -7,5 +10,39 @@ namespace WebAPI.Controllers.Concrete
     [ApiController]
     public class AuthController : ControllerBase
     {
+        IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userLoginStat = await _authService.Login(userForLoginDto);
+            if (!userLoginStat.Success)
+            {
+                return BadRequest();
+            }
+            var token = await _authService.CreateAccessToken(userLoginStat.Data);
+            return token.Success
+                ? Ok(token.Message)
+                : BadRequest(token.Message);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto, string password)
+        {
+            var registerStat = await _authService.Register(userForRegisterDto, password);
+            if (!registerStat.Success)
+            {
+                return BadRequest(registerStat.Success);
+            }
+            var token = await _authService.CreateAccessToken(registerStat.Data);
+            return token.Success
+                ? Ok(token.Data) //data change to message later
+                : BadRequest(token.Message);
+        }
     }
 }
