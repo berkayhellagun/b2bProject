a12b372e68a04f5c9621bed02c87a8b96d236851
berkayhellagun <berkayhellagun@hotmail.com>
2022-07-13 15:43:58 +0300

    ioc integretion for dependency

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,23 @@
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.MemoryCaching;
+using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DependencyResolvers
+{
+    // this class dependency resolver for core module.
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSingleton<ICacheService, MemoryCacheManager>();
+
+        }
+    }
+}
