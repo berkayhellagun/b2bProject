5f4c3a7163ff2e5bb41eae6030752d95fe93352b
Berkay Hellagün <berkayhellagun@hotmail.com>
2023-06-19 15:33:57 +0300

    fix frontend

---
+++ WebMVC/Models/MostOrderedProductModel.cs
@@ -0,0 +1,25 @@
+﻿namespace WebMVC.Models
+{
+    public class MostOrderedProductModel
+    {
+        public ProductDetail Item1 { get; set; }
+        public int Item2 { get; set; }
+    }
+
+    public class ProductDetail
+    {
+        public string ProductName { get; set; }
+        public object ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductInStock { get; set; }
+        public List<Property> Properties { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class Property
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public int Id { get; set; }
+    }
+}
--- WebMVC/Models/ProductModel.cs
+++ WebMVC/Models/ProductModel.cs
@@ -2,7 +2,7 @@
 {
     public class ProductModel
     {
-        public int ProductId { get; set; }
+        public int Id { get; set; }
         public string ProductName { get; set; }
         public int ProductSupplierId { get; set; }
         public int ProductCategoryId { get; set; }
